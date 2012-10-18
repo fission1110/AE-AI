75230ffdd512be8e61a0e906d58dd8fc399d378f
fission1110 <rysnxedge1110@gmail.com>
2012-10-18 13:15:46 -0600

    Cleaned up a ton of code. I fixed some things, did some refactoring and cleaning up. I made it so that you can spawn up different mainloop processes making multithreading easier. I also killed off alot of the std output.

--- main.py
+++ main.py
@@ -4,26 +4,51 @@ from evolve.callbacks import define
 import thread
 
 
-def mainloop(input, expected_output, iterations = 10000, bf_starting_code = ''):
+def mainloop(input, expected_output, iterations = 10000, starting_code = '', stdout = False):
     """ The main loop """
     ai = obj()
     define(ai)
-    ai.set(bf_starting_code)
+    ai.set(starting_code)
     for i in range(0,iterations):
-        ai.set_memory(input)
+        starting_string = input()
+        target_string = expected_output()
+        ai.set_memory(starting_string)
         ai.execute()
+        if stdout and i%5000 == 0:
+            print '---------------------------------------------------------------'
+            print 'iteration number: ' + str(i)
+            print 'champ instructions: ' + ai.champ_instructions
+            print 'champ score: ' + str(ai.champ_score)
+            print 'champ target: ' + target_string
+            print 'champ input: ' + starting_string
+            print 'champ current: ' + ai.champ_output
 
-        ai.test(expected_output)
+        ai.test(target_string)
         # once every 1000 iterations, revert the instructions to the champion
         if i %2000 == 0 and i>10:
             ai.instructions = ai.champ_instructions
-    print ai.champ_score
+    if stdout:
+        print 'champ code: ' + ai.champ_instructions
     return ai
-try:
-       thread.start_new_thread( mainloop, ("aaaaaa", 'bbbbb' ) )
-       thread.start_new_thread( mainloop, ("aaaaaa", 'bbbbb' ) )
-except:
-       print "Error: unable to start thread"
-while 1:
-    pass
+
+def input():
+    return 'abcdefghijklmnopqrstuvwxyz'
+def expected_output():
+    return 'zyxwvutsrqponmlkjihgfedcba'
+mainloop(input, expected_output, 10000000, '', True) 
+
+
+
+
+
+
+
+#Multi threading!
+#try:
+#       thread.start_new_thread( mainloop, (input, expected_output, 10000, '', True) )
+#       thread.start_new_thread( mainloop, (input, expected_output, 10000, '', True ) )
+#except:
+#       print "Error: unable to start thread"
+#while 1:
+#    pass
 
